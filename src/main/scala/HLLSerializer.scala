4e407a3f1c29a0a12fdd64d0cbf0a108ae996e63
bigsnarfdude <ohprecio@gmail.com>
2015-10-22 12:28:29 -0700

    blah

---
+++ src/main/scala/HLLSerializer.scala
@@ -0,0 +1,25 @@
+import com.twitter.algebird.HyperLogLog
+import com.twitter.algebird.HyperLogLogMonoid
+import com.twitter.algebird.HLL
+import org.apache.commons.codec.binary.Base64
+
+object HLLSerializer {
+
+    def decodeString(hllHash:String): com.twitter.algebird.HLL = {
+      // decode the string into bytes
+      val bytes = Base64.decodeBase64(hllHash)
+      // hyperll is a Hyperloglog data structure
+      val hyperll = kryo.fromBytes(bytes).asInstanceOf[HLL]
+      // return hyperll
+      hyperll
+    }
+
+    def encodeString(hll: HLL): String = {
+      // takes hll and converts to bytes
+      val bytes: Array[Byte] = kryo.toBytesWithClass(hll)
+      // encode to Base64
+      val encoded = Base64.encodeBase64(bytes)
+      // create String object as Base64 encoded HLL
+      new String(encoded)
+    }
+}
